51c6e0069459a17f2ba39681ebbc6e8d76e75703
ajiayi-debug <e0774928@u.nus.edu>
2025-09-07 00:08:23 +0800

    gimme a moment

--- agents/image_generation/visualise.py
+++ agents/image_generation/visualise.py
@@ -5,7 +5,13 @@ from nanobanana import gemini_generate_images
 
 
 
-async def visualize_generated_images(prompt: str):
+async def find_generate_images(prompt: str):
     await call_agent_async(gemini_generate_images, prompt)
     all_images = [str(p) for p in Path("downloads").rglob("*") if p.suffix.lower() in {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"}]
     image_paths = random.sample(all_images, min(5, len(all_images)))
+    images=[]
+    for img_path in image_paths:
+        images.append(img_path)
+
+
+    
